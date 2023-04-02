f589fd20c68d2958485f1d94b2ebd142493097ee
Wiraphong2003 <111116563+Wiraphong2003@users.noreply.github.com>
2023-04-03 04:02:24 +0700

    as

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const mysql = require('mysql')
 const app = express();
 app.use(express.json())
 
+
+
+app.use(express.urlencoded({ extended: true }));
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");
     // res.header('Access-Control-Allow-Origin', "http://localhost:4200");
