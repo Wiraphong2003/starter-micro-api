31e85cdebe80ae37efb166a18200856ff889c72d
Wiraphong2003 <111116563+Wiraphong2003@users.noreply.github.com>
2023-04-03 03:18:51 +0700

    as

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const app = express();
 app.use(express.json())
 
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Origin', "http://localhost:4200/");
+    // res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Origin', "http://localhost:4200");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', "Content-Type");
     next();
